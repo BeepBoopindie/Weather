c512d09442a7c86c8fa517efe3994b559d43ba38
Ross <59945702+BeepBoopindie@users.noreply.github.com>
2022-05-13 00:13:25 +0100

    location Search
    Added in location search with results displayed

--- main.js
+++ main.js
@@ -2,8 +2,66 @@
 //Design by https://www.behance.net/gallery/139988031/Weather-Mobile-App-Design?tracking_source=search_projects_recommended%7Cweather%20app 
 //google api key AIzaSyCmFOpufylZd55_74qd4YruSQ5037u9BUM
 const key = "50745880bac2336ee9d7bb568346f4db";
-const _lat = "51.3903102";
-const _long = "1.3958178";
+var _lat = "51.3903102";
+var _long = "1.3958178";
+
+
+const getLocation  = async()  =>{
+    console.log("test");
+    const location = document.getElementById("locsearch").value;
+    const baseUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+    const query = `${location}&limit=5&appid=${key}`;
+
+    const res = await fetch(baseUrl + query);
+    const data = await res.json();
+    console.log(data);
+
+    for(let i =0; i < 5; i++){
+        if(!data){
+            document.getElementById("searchresult").style.left= "900";
+            return;
+        }
+        document.getElementById("searchresult").style.left= "0";
+        if(data[i]){
+            console.log("sr-"+ i);
+            var spans = document.getElementById("sr-"+ i).getElementsByTagName('span');
+            document.getElementById("sr-"+ i).setAttribute("lat", data[i].lat);
+            document.getElementById("sr-"+ i).setAttribute("lon", data[i].lon);
+            document.getElementById("sr-"+ i).setAttribute("placename", data[i].name);
+            spans[0].innerHTML= data[i].name;
+            spans[1].innerHTML = data[i].state + ", " + data[i].country;
+        }else{
+            var spans = document.getElementById("sr-"+ i).getElementsByTagName('span');
+            document.getElementById("sr-"+ i).syle = "pointer-events: none;";
+            spans[0].innerHTML= "";
+            spans[1].innerHTML = "";
+        }
+    }
+
+    return data;
+}
+
+function opensearch(){
+    document.getElementById("search").style.display = "block";
+    document.getElementById("Mainweather").style.display = "none";
+}
+
+function closeSearch(){
+    closeNav();
+    document.getElementById("search").style.display = "none";
+    document.getElementById("Mainweather").style.display = "block";
+}
+
+function selectCity(id){
+    var elem = document.getElementById("sr-"+ id);
+    var lat = elem.getAttribute("lat");
+    var lon = elem.getAttribute("lon");
+    var placename =  elem.getAttribute("placename");
+    document.getElementById("location").innerHTML = '<i class="fa-solid fa-location-dot"></i> ' + placename;
+    console.log(lat + "| " + lon);
+    closeSearch();
+    getweather(lat,lon);
+}
 
 const getweather = async(lat,long) => {
     const baseUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=";
@@ -11,6 +69,9 @@ const getweather = async(lat,long) => {
 
     const res = await fetch(baseUrl + query);
     const data = await res.json();
+
+    _lat = lat;
+    _long = long;
     console.log(data);
     UpdateUI(data);
     //UpdateUI(data[0]);
@@ -19,7 +80,7 @@ const getweather = async(lat,long) => {
 
 function UpdateUI(data){
 
-    let d = new Date(data.current.dt * 1000);
+    let d = new Date((data.current.dt + data.timezone_offset - 3600) * 1000);
     console.log(d.getHours());
     if(d.getHours() > 6 && d.getHours() < 19){
         document.body.style.backgroundImage=  "linear-gradient(140deg,#1404ba, #E3BEFE)"; 
@@ -43,7 +104,7 @@ function UpdateUI(data){
     //Hourly UI
 
     for(let i =0; i < 12; i++){
-        document.getElementById("t" + (i+1) + "-time").innerText = new Date(data.hourly[i].dt * 1000).toLocaleString('en-gb', {
+        document.getElementById("t" + (i+1) + "-time").innerText = new Date((data.hourly[i].dt+ data.timezone_offset - 3600) * 1000).toLocaleString('en-gb', {
             hour: 'numeric', // numeric, 2-digit
             minute: 'numeric', // numeric, 2-digit
         });
@@ -90,18 +151,47 @@ function setSunset (){
     ctx.stroke();
 }
 
-function getMap(){
+function getMap(lat, long){
    
     document.getElementById("map").src='https://maps.googleapis.com/maps/api/staticmap?center=' + _lat +',' + _long + '&zoom=12&size=400x200&key=AIzaSyCmFOpufylZd55_74qd4YruSQ5037u9BUM';
         
 }
 
-function search_Location(){
+function save_Location(){
 
 }
 
-function save_Location(){
 
+function openinfo() {
+
+    var wb = document.getElementById("weatherbox");
+    var btntxt = document.getElementById("btn-txt");
+    if (wb.style.height === "800px") {
+        wb.style.height = "180px";
+        document.body.style.height= "100vh";
+        btntxt.innerText = "See More";
+    } else {
+        wb.style.height = "800px";
+        document.body.style.height=  "auto";
+        btntxt.innerText = "See Less";
+    }
+
+  
+  const d = new Date();
+    let hour = d.getHours();
+
+    if(hour> 6 && hour <= 19){
+        document.body.style.backgroundImage=  "linear-gradient(140deg,#1404ba, #E3BEFE)"; 
+    }else{
+        document.body.style.backgroundImage=  "linear-gradient(140deg,#0C0214, #7744CA)"; 
+    }
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 
